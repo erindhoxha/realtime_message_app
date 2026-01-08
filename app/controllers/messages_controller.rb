0f0e62dedfef0018e75af9c99a613793528155ab
erindhoxha <hello@erindhoxha.dev>
2026-01-08 01:29:23 +0100

    Adding the messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
   end
 
   def message_render(message)
-    render(partial: 'message', locals: {message: message})
+    ApplicationController.renderer.render(partial: 'message', locals: { message: message })
   end
 
 end
